831743bc67d5b8ea76cfb99d39d6374d7342a3da
Kat Marchán <kzm@zkat.tech>
2019-06-18 15:58:33 -0700

    misc: cargo fmt

--- src/compute.rs
+++ src/compute.rs
@@ -1,13 +1,13 @@
 use std::ffi::OsStr;
+use std::fs::File;
+use std::io::{BufReader, Read, Write};
 #[cfg(unix)]
 use std::os::unix::ffi::OsStrExt;
 #[cfg(windows)]
 use std::os::windows::ffi::OsStrExt;
-use std::fs::File;
-use std::io::{BufReader, Read, Write};
 
 use clap::ArgMatches;
-use ssri::{Builder, Integrity, Algorithm};
+use ssri::{Algorithm, Builder, Integrity};
 
 pub fn compute(matches: ArgMatches) {
     let files = matches
@@ -33,7 +33,8 @@ pub fn compute(matches: ArgMatches) {
                     #[cfg(windows)]
                     let output: Vec<u8> = f.encode_wide().collect();
 
-                    std::io::stdout().write_all(&output[..])
+                    std::io::stdout()
+                        .write_all(&output[..])
                         .expect("failed to write out filename");
                     println!();
                 }
@@ -55,10 +56,10 @@ fn hash_file(f: &OsStr, matches: &ArgMatches) -> Result<Integrity, std::io::Erro
             "sha256" => Algorithm::Sha256,
             "sha384" => Algorithm::Sha384,
             "sha512" => Algorithm::Sha512,
-            _ => panic!("bad algorithm: {}", algo)
+            _ => panic!("bad algorithm: {}", algo),
         };
         builder = builder.algorithm(algo);
-    };
+    }
 
     if f == OsStr::new("-") {
         read_from_file(BufReader::new(std::io::stdin()), &mut builder)?;
@@ -68,7 +69,10 @@ fn hash_file(f: &OsStr, matches: &ArgMatches) -> Result<Integrity, std::io::Erro
     Ok(builder.result())
 }
 
-fn read_from_file<T: Read>(mut reader: BufReader<T>, builder: &mut Builder) -> Result<(), std::io::Error> {
+fn read_from_file<T: Read>(
+    mut reader: BufReader<T>,
+    builder: &mut Builder,
+) -> Result<(), std::io::Error> {
     let mut buf = [0; 1024 * 256];
     loop {
         let amt = reader.read(&mut buf)?;
